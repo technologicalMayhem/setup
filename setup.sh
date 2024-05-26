271ac7d27ab9842e91a4f44ae515dd2daac55133
Tobias Freese <wolfshund98@gmail.com>
2024-05-26 13:36:23 +0200

    Do not run as root

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ###                        ###
 
 # Remove unecessary packages
-pacman -R --noconfirm gnome-console xterm
+sudo pacman -R --noconfirm gnome-console xterm
 
 # Install yay
 if ! pacman -Q yay; then
-  pacman -Sy --needed git base-devel
+  sudo pacman -Sy --needed git base-devel
   git clone https://aur.archlinux.org/yay.git
   cd yay
   makepkg -si
@@ -31,9 +31,6 @@ com.github.tchx84.Flatseal
 #  Configure packages  #
 ###                  ###
 
-echo "Disable eos-welcome showing up on boot"
-sed -i '/Hidden=false/c\Hidden=true' /etc/xdg/autostart/welcome.desktop
-
 echo "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -41,7 +38,7 @@ echo "Install p10k"
 zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k'
 sed -i '/ZSH_THEME=/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
 
-echo "Confgiure git"
+echo "Configure git"
 git config --global user.email "wolfshund98@gmail.com"
 git config --global user.name "Tobias Freese" 
 git config --global init.defaultBranch "main"
